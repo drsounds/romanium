1b60431845e861c4e41b87c9f8e2c519621306b8
Dr. Sounds <management@dr-sounds.com>
2017-04-11 13:31:44 +0000

    T

--- client/js/main.js
+++ client/js/main.js
@@ -18,7 +18,11 @@ function setScene(scene) {
     });
    setTimeout(function () {
      document.querySelector('#scene').setAttribute('src', '#' + scene.id);
+    if (scene.text instanceof Array) {
+      writeTextSequence(scene.text)
+    } else {
      writeText(scene.text);
+    }
    }, 3000)
 }
 function writeText(text, cb) {
@@ -29,7 +33,9 @@ function writeText(text, cb) {
   var xs = setInterval(function () {
     if (buffer >= text) {
       clearInterval(xs);
-      if (cb instanceof Function) cb();
+      setTimeout(function () {
+        if (cb instanceof Function) cb();
+      }, 2000);
     }
     var char = text[i];
     if (char)
@@ -40,34 +46,46 @@ function writeText(text, cb) {
   }, 100);
 }
 function writeTextSequence(texts, duration, cb) {
-  
-  texts.map(function (text, i) {
-    setTimeout(function () {
-      writeText(text);    
-    }, duration * i);
-  })
+
+  var i = 0;  
+  function at () {
+    var text = texts[i];
+    writeText(text, at);    
+    i++; 
+    if (i == texts.length - 1) {
+      setTimeout(function () {
+        cb();
+      }, 1000);
+    }
+  };
+  at();
 }
 var scenes = [
   {
     id: 'orphanage_office',
-    text: _('orphanage_office')
+    text: _('orphanage_office'),
+    duration: 5000
   },
   {
     id: 'sleep_with_mother',
-    text: _('sleep_with_mother')
+    text: _('sleep_with_mother'),
+    duration: 5000
     
   },
   {
     id: 'spaceship',
-    text: _('spaceship')
+    text: _('spaceship'),
+    duration: 5000
   },
   {
     id: 'sleep_in_own_bed',
-    text: _('sleep_in_own_bed')
+    text: _('sleep_in_own_bed'),
+    duration: 5000
   },
   {
     id: 'flag_slide',
-    text: _('flag_slide')
+    text: _('flag_slide'),
+    duration: 5000
   }
 ];
 function showScenes() {
@@ -99,16 +117,26 @@ function showSpace() {
       easing: 'linear'
     });
 }
-var SEQUENCE_START = 18000;
+var SEQUENCE_START = 38000;
 var SLIDESHOW_START = 3000;
 var TRANSITION_LENGTH = 3000;
 var SCENE_LENGTH = 10000;
 window.addEventListener('load', function () {
   writeTextSequence([
-    _('intro1'),
-    _('intro2')
-  ],
-  6000);
+    1,2,3,4,5,6,7,8,9,10
+  ].map(function (i) {
+    return _('intro' + i);
+  }),
+  2000);
+  var f = 0;
+  var sphere = document.querySelector('#sphere');
+  setInterval(function () {
+    if (f == 3) {
+      f = 0;
+    }
+    sphere.setAttribute('src', '#sphere' + f + '');
+    f++;
+  }, 300);
   setTimeout(function () {
     showScenes();
     setTimeout(function () {
@@ -116,10 +144,11 @@ window.addEventListener('load', function () {
     }, SLIDESHOW_START + SCENE_LENGTH * scenes.length)
     setTimeout(function () {
       
-      
+      var l = 0;
       scenes.map(function (scene, i) {
         setTimeout(function () {
           setScene(scene);
+          l += (scene.duration) * (i) 
         }, SCENE_LENGTH * (i));
         
       });
